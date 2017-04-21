bc06fac4d663286e5d0cbfe326cbcc3f80c290ce
Ruben Sissing <ruben@sissing.nl>
2017-04-21 12:34:13 +0100

    setup CoreData

---
+++ DreamLister/Image+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Image+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Ruben Sissing on 21/04/2017.
+//  Copyright © 2017 Ruben Sissing. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Image: NSManagedObject {
+
+}
