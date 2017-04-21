fdc75aa95bada837019c1f76581c2cb0fda8b722
Ruben Sissing <ruben@sissing.nl>
2017-04-21 12:59:54 +0100

    add TableView

---
+++ DreamLister/MaterialView.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialView.swift
+//  DreamLister
+//
+//  Created by Ruben Sissing on 21/04/2017.
+//  Copyright © 2017 Ruben Sissing. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
