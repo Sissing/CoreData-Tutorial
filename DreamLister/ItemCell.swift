1b8040df94baabac0c5cce414c55dd28cbdfd140
Ruben Sissing <ruben@sissing.nl>
2017-04-21 13:57:44 +0100

    add IBOutlets

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Ruben Sissing on 21/04/2017.
+//  Copyright © 2017 Ruben Sissing. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet var thumb: UIImageView!
+    @IBOutlet var title: UILabel!
+    @IBOutlet var price: UILabel!
+    @IBOutlet var details: UILabel!
+    
+
+}
